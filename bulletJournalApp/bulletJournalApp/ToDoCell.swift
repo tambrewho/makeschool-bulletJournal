e1bf195376922e3ae43f04417a6268918be29109
Tambre Hu <thu.1215@gmail.com>
2017-07-27 12:54:11 -0400

    ToDoCell

---
+++ bulletJournalApp/bulletJournalApp/ToDoCell.swift
@@ -0,0 +1,27 @@
+//
+//  ToDoCell.swift
+//  bulletJournalApp
+//
+//  Created by Tambre Hu on 7/26/17.
+//  Copyright © 2017 Make School. All rights reserved.
+//
+
+import UIKit
+
+class ToDoCell: UITableViewCell {
+
+    @IBOutlet weak var weekdayLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
+  
